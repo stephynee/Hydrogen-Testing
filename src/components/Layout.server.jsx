a462ef5a3355138ceda302e11112bb071a51fc3f
Stephanie <stephykins@gmail.com>
2022-09-23 13:16:03 -0400

    Add SEO component

--- src/components/Layout.server.jsx
+++ src/components/Layout.server.jsx
@@ -1,4 +1,4 @@
-import { useShopQuery, CacheLong, gql, useUrl, Link } from "@shopify/hydrogen";
+import { useShopQuery, CacheLong, gql, useUrl, Link, Seo } from "@shopify/hydrogen";
 import { Suspense } from "react";
 
 const Layout = ({ children }) => {
@@ -13,6 +13,15 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Suspense>
+        <Seo
+          type="defaultSeo"
+          data={{
+            title: shop.name,
+            description: shop.description,
+          }}
+        />
+      </Suspense>
       <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
         <div className="">
           <a href="#mainContent" className="sr-only">
