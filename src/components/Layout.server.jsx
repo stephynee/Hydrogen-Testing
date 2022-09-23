bf6fc96c2dbaef363b34d7c9d96bfdc5f636ed1d
Stephanie <stephykins@gmail.com>
2022-09-23 13:00:56 -0400

    Add components for page layout and for featured collections.

---
+++ src/components/Layout.server.jsx
@@ -0,0 +1,52 @@
+import { useShopQuery, CacheLong, gql, useUrl, Link } from "@shopify/hydrogen";
+import { Suspense } from "react";
+
+const Layout = ({ children }) => {
+  const { pathName } = useUrl();
+  const isHome = pathName === '/';
+
+  const { data: { shop },} = useShopQuery({
+    query: SHOP_QUERY,
+    cache: CacheLong(),
+    preload: true,
+  });
+
+  return (
+    <>
+      <div className="flex flex-col min-h-screen antialiased bg-neutral-50">
+        <div className="">
+          <a href="#mainContent" className="sr-only">
+            Skip to content
+          </a>
+        </div>
+        <header
+          role="banner"
+          className={`flex items-center h-16 p-6 md:p-8 lg:p-12 sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 antialiased transition shadow-sm ${
+            isHome ? "bg-black/80 text-white" : "bg-white/80"
+          }`}
+        >
+          <div className="flex gap-12">
+            <Link className="font-bold" to="/">
+              {shop.name}
+            </Link>
+          </div>
+        </header>
+
+        <main role="main" id="mainContent" className="flex-grow">
+          <Suspense>{children}</Suspense>
+        </main>
+      </div>
+    </>
+  );
+};
+
+const SHOP_QUERY = gql`
+  query ShopInfo {
+    shop {
+      name
+      description
+    }
+  }
+`;
+
+export default Layout;
